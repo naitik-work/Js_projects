2eac2951078a19f42862b15b0edf8643cde93103
naitik-cmd <naitikchs16@gmail.com>
2025-12-16 21:55:56 +0530

    controlls

--- Snake-Game/script.js
+++ Snake-Game/script.js
@@ -5,6 +5,7 @@ const blockWidth=50;
 const rows= Math.floor(board.clientHeight / blockHeight);
 const cols= Math.floor(board.clientWidth / blockWidth);
 
+let intervalId= null;
 const blocks= [];
 
 // for(let i =0; i<rows*cols; i++){
@@ -12,6 +13,12 @@ const blocks= [];
 //     block.classList.add('block');
 //     board.appendChild(block);
 // }
+
+const snake= [{
+  x:1, y:3
+}]
+
+let direction= 'left';
 
 
 for(let row=0; row<rows; row++){
@@ -23,3 +30,54 @@ for(let row=0; row<rows; row++){
         blocks[`${row}, ${col}`]=block;
     }
 }
+
+function render(){
+    snake.forEach(segment=>{
+        blocks[`${segment.x}, ${segment.y}`].classList.add('fill');
+    })
+}
+
+intervalId=setInterval(()=>{
+    let head=null;
+    if(direction==='left'){
+        head={x: snake[0].x, y: snake[0].y -1};
+    }
+    else if(direction==='right'){
+        head={x: snake[0].x, y: snake[0].y +1};
+    }
+    else if(direction==='up'){
+        head={x: snake[0].x -1, y: snake[0].y};
+    }
+    else if(direction==='down'){
+        head={x: snake[0].x +1, y: snake[0].y};
+    }
+
+    if(head.x<0 || head.x>=rows || head.y<0 || head.y>=cols){
+        alert('Game Over');
+        clearInterval(intervalId);
+    }
+
+    snake.forEach(segment=>{
+        blocks[`${segment.x}, ${segment.y}`].classList.remove('fill');
+    })
+    snake.unshift(head);
+    snake.pop();
+    render(); 
+
+
+},300);
+
+addEventListener('keydown', e=>{
+    if(e.key==='ArrowLeft'){
+        direction='left';
+    }
+    else if(e.key==='ArrowRight'){
+        direction='right';
+    }
+    else if(e.key==='ArrowUp'){
+        direction='up';
+    }
+    else if(e.key==='ArrowDown'){
+        direction='down';
+    }
+});
